7bbbc1b98178043ca03ce9add0e5181ed3eb1879
Noelia Ibáñez <49150688+Nibanez0@users.noreply.github.com>
2021-03-14 22:33:52 +0000

    Delete ejercicio-3.d.ts

--- dist/ejercicio-3.d.ts
+++
@@ -1,8 +0,0 @@
-declare function meanAndConcatenate(array: (number | string[]), [number, string]: [any, any], { let, numeros: number, let, media: number, let, cadena: string, array, forEach }: {
-    let: any;
-    numeros?: number;
-    media?: number;
-    cadena?: string;
-    array: any;
-    forEach: any;
-}): any;
